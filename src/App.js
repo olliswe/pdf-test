bcee0adfa62921523bad12b41f8fcb462cc9535e
Oliver Iyer <oliver.iyer@doctolib.com>
2021-11-18 17:10:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import {Document, Page} from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import ReactJson from "react-json-view";
+
 
 function App() {
+  const [annotations, setAnnotations] = useState()
+  const [formValues, setFormValues] =useState()
+
+    useEffect(() => {
+        if(!annotations) return
+        setFormValues(annotations.map((annotation)=>({
+            fieldName:annotation.fieldName,
+            fieldId:annotation.id,
+            fieldType:annotation.fieldType,
+            fieldValue:annotation.fieldValue,
+            dimensions: annotation.rect
+        })))
+    }, [annotations]);
+
+    useEffect(() => {
+        if(!annotations) return
+        annotations.forEach(({id})=>{
+            const inputEl = document.getElementById(id)
+            console.log(inputEl)
+
+            inputEl.addEventListener('input', (e)=>{
+                console.log('foo') // THIS EVENT LISTENER IS NOT BEING ATTACHED :(((
+                setFormValues(prev=>{
+                    const currentFormValueIndex = prev.indexOf(x=>x.id ===id)
+                    const currentFormValue = prev[currentFormValueIndex]
+                    const newFormValues = prev.slice().splice(1, currentFormValueIndex, {...currentFormValue, fieldValue:e.target.value} )
+                    return newFormValues
+                })
+            })
+        })
+    }, [annotations]);
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{display:'flex'}}>
+        <Document
+            file={`${window.location.origin}/auform.pdf`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Page pageNumber={1} renderInteractiveForms={true} onGetAnnotationsSuccess={(annotations)=>setAnnotations(annotations)}/>
+        </Document>
+          <ReactJson src={formValues} collapsed={true}/>
+      </div>
   );
 }
 
-export default App;
+export default App
